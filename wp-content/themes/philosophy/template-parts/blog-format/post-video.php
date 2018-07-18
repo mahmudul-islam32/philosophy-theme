5d8bb10a7dadfe4e3e40eac40346149e97bcdd93
Mahmudul islam <nildiganta32@gmail.com>
2018-07-18 05:03:59 +0000

    process blog home page posts

--- wp-content/themes/philosophy/template-parts/blog-format/post-video.php
+++ wp-content/themes/philosophy/template-parts/blog-format/post-video.php
@@ -4,8 +4,7 @@
                         
                     <div class="entry__thumb video-image">
                         <a href="https://player.vimeo.com/video/117310401?color=01aef0&title=0&byline=0&portrait=0" data-lity>
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/thumbs/masonry/shutterbug-400.jpg" 
-                                    srcset="<?php echo get_template_directory_uri(); ?>/assets/images/thumbs/masonry/shutterbug-400.jpg 1x, <?php echo get_template_directory_uri(); ?>/assets/images/thumbs/masonry/shutterbug-800.jpg 2x" alt="">
+                            <?php the_post_thumbnail("philosophy-home-square"); ?>
                         </a>
                     </div>
                  <?php get_template_part('template-parts/common/blog/common-post'); ?>

32002c1c5ac9de7a00f42395f328341abbeac387
Mahmudul islam <nildiganta32@gmail.com>
2018-07-26 05:37:19 +0000

    add submenu class using string replace

--- wp-content/themes/philosophy/template-parts/common/navigation.php
+++ wp-content/themes/philosophy/template-parts/common/navigation.php
@@ -7,12 +7,15 @@
 
                     <?php 
                     
-                    wp_nav_menu(array(
+                  $philoshophy_menu =  wp_nav_menu(array(
                         
                         'theme_location' =>'topmenu',
                         'menu_id' => 'topmenu',
-                        'menu_class' => 'header__nav'
-                        ))
+                        'menu_class' => 'header__nav',
+                        'echo' => false
+                        ));
+                      $philoshophy_menu = str_replace("menu-item-has-children" ,"menu-item-has-children has-children", $philoshophy_menu); 
+                      echo $philoshophy_menu;
                     
                     ?>
 

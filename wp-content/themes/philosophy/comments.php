6e02f1dadac4d4c7afe6b8ae5353706a96ba0b62
Mahmudul islam <nildiganta32@gmail.com>
2018-07-29 06:47:13 +0000

    Add comment form

---
+++ wp-content/themes/philosophy/comments.php
@@ -0,0 +1,43 @@
+        <div class="comments-wrap">
+
+            <div id="comments" class="row">
+                <div class="col-full">
+
+                    <h3 class="h2">
+                    
+                     <?php 
+                     
+                     $philosophy_cn = get_comments_number();
+                     
+                     if($philosophy_cn <= 1){
+                         echo $philosophy_cn." ".__("Comment", "philosophy");
+                     }else{
+                        echo $philosophy_cn." ".__("Comments", "philosophy");
+                     }
+                     
+                     ?>
+                    
+                    </h3>
+
+                    <!-- commentlist -->
+                    <ol class="commentlist">
+
+                    <?php wp_list_comments(); ?>
+
+                    </ol> <!-- end commentlist -->
+
+
+                    <!-- respond
+                    ================================================== -->
+                    <div class="respond">
+
+                        <h3 class="h2"><?php _e("Add Comment", "philosophy");?></h3>
+
+                      <?php comment_form(); ?>
+
+                    </div> <!-- end respond -->
+
+                </div> <!-- end col-full -->
+
+            </div> <!-- end row comments -->
+        </div> <!-- end comments-wrap -->
